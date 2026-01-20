7a71038ed2b6e49ddea176b9b2a7d2a18d9627bf
wangsiqiwsq <1477634418@qq.com>
2026-01-20 16:35:10 +0800

    Create sy2_8(reverse).c

---
+++ 实验2/sy2_8(reverse).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char string[100];
+    char reverse[100];
+    int size;
+    printf("请输入一个整数，之后会将它反转并输出：\n");
+    scanf("%s",string);
+    size=strlen(string);
+    for(int i=0;i<=size-1;i++){
+        reverse[size-i-1]=string[i];
+    }
+    reverse[size]=0;
+    printf("反转后为：%s\n",reverse);
+    return 0;
+}
